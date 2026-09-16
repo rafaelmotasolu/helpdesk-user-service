b07d43a8e3fbf3fa0b1835c8caf5c88249d380f8
rafaelmotacr <rafaelmota361@gmail.com>
2026-09-15 21:25:39 -0300

    fix: Renames the "usuarios" microservice to "user".

---
+++ src/main/java/com/solutis/projeto/helpdesk_user_service/HelpdeskUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solutis.projeto.helpdesk_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutis/projeto/helpdesk_user_service/HelpdeskUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutis.projeto.helpdesk_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
